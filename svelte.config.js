5315de89bb6b199dfaccd8df9e842fef6aac86ed
vytker <vytkerpsn@gmail.com>
2025-05-08 11:50:26 +0200

    cambio_static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter({fallback: 'app.html'}) } };
 
 export default config;
