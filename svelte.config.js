63963015030c7795b48e716154e4ebcf800e13b5
vytker <vytkerpsn@gmail.com>
2025-05-08 12:04:53 +0200

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter({fallback: 'app.html'}) } };
+const config = { kit: { adapter: adapter({
+    pages: 'dist',
+    assets: 'dist',
+    fallback: 'app.html'}) } };
 
 export default config;
